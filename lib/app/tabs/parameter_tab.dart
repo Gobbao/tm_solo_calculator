7a41865927c15fc29e551faa17bae65e5ca71ced
Gabriel Gobbi <gobbao@hotmail.com>
2020-06-03 23:10:22 -0300

    Remove duplicate instanciations of state

--- lib/app/tabs/parameter_tab.dart
+++ lib/app/tabs/parameter_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../app_state.dart';
 import '../components/parameter_card.dart';
@@ -6,7 +7,7 @@ import '../components/parameter_card.dart';
 class ParameterTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var state = AppState();
+    final state = Provider.of<AppState>(context, listen: false);
 
     return ListView(
       children: List.from([state.generation])
--- lib/app/tabs/resource_tab.dart
+++ lib/app/tabs/resource_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../app_state.dart';
 import '../components/resource_card.dart';
@@ -6,7 +7,7 @@ import '../components/resource_card.dart';
 class ResourceTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var state = AppState();
+    final state = Provider.of<AppState>(context, listen: false);
 
     return ListView(
       children: state.resources.keys
